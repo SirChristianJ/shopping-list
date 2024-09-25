29c2da45185ff7020b96a724ff8eea3bf501686e
SirChristianJ <git@github.com:SirChristianJ/todoApp.git>
2024-09-25 15:34:12 -0400

    New items in shopping list

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -4,7 +4,7 @@ public class ShoppingList {
     public static void main(String[] args){
         System.out.println("Shopping List");
         System.out.println("-------------------");
-        String[] shopList = {"Apples","Bananas","Cherries","DinoNuggies"};
+        String[] shopList = {"Apples","Bananas","Cherries","DinoNuggies","Eggs","Fanta","Grapes","Honeybuns","Icing","Jerky"};
         for(int i = 0; i < shopList.length;i++){
             System.out.println(shopList[i]);
         }
